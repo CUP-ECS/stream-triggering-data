d3d29f17421af4268922d6df265ee88eb7044ca7
Derek Schafer <schaferderek96@gmail.com>
2025-11-05 06:46:55 -0800

    Saving new files, not updated to work in new location yet

---
+++ run-scripts/TUOLUMNE/flux-tuo-run-multi.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#flux: --nodes=2
+#flux: --nslots=8
+#flux: --gpus-per-slot=1
+#flux: --output=TUO-{{nnodes}}.out
+#flux: --exclusive
+#flux: --time-limit=10m
+#flux: --queue=pdebug
+#flux: --env=FLUX_TEST={{nnodes}}
+
+#ulimit -c unlimited
+
+NODES=$FLUX_TEST
+
+SYSTEM=TUO
+export HSA_XNACK=1
+module load craype-accel-amd-gfx942
+module load rocm
+
+COLLECTION_DIR=outputs
+FILENAME_BASE="./$COLLECTION_DIR/$SYSTEM-$NODES-$(date +%m-%d)"
+COUNT=1
+TARGET="${FILENAME_BASE}-${COUNT}.out"
+
+while [[ -e $TARGET ]]; do
+    ((COUNT++))
+    TARGET="${FILENAME_BASE}-${COUNT}.out"
+done
+
+touch "$TARGET"
+echo $TARGET
+
+HOSTNAMES_FILE="$COLLECTION_DIR/0-hostnames.tmp"
+VAR_MOD_FILE="$COLLECTION_DIR/0-var-mod.tmp"
+module list >> $VAR_MOD_FILE 2>&1
+srun --nodes=$NODES --ntasks-per-node=1 --output=$HOSTNAMES_FILE hostname
+
+TEST="./build/src/gol"
+
+START_EXP=0
+END_EXP=2
+TIME=2m
+ITERS=1000
+
+run_test()
+{
+    RUN_FILE="$COLLECTION_DIR/$1.tmp"
+    STRING="Test: ${2} $NODES $PPN $SIZE"
+    if [[ "$1" == "mpich" ]]; then
+        flux run -N$NODES --tasks-per-node=$PPN --output="$RUN_FILE" $TEST -n $SIZE -c mpi -t $ITERS
+    else
+        flux run -N$NODES --tasks-per-node=$PPN --output="$RUN_FILE" $TEST -n $SIZE -c mpi-advance -t $ITERS
+    fi
+    sed -i "1i$STRING" $RUN_FILE
+}
+
+for (( exp=START_EXP; exp<=END_EXP; exp++ )); do
+    PPN=$((2 ** $exp))
+    for j in {14..14}; do
+        SIZE=$((2 ** $j))
+        SIZE=16384
+
+        run_test "a-db" "MPI Advance Double Buffer"
+
+        export MPI_ADVANCE_DOUBLE_BUFFERING=0
+        run_test "a-sb" "MPI Advance Single Buffer"
+        unset MPI_ADVANCE_DOUBLE_BUFFERING
+
+        export MPI_ADVANCE_FINEGRAIN_MEMORY=1
+        run_test "a-fg-db" "MPI Advance Double Fine-grained Buffer"
+        export MPI_ADVANCE_DOUBLE_BUFFERING=0
+        run_test "a-fg-sb" "MPI Advance Single Fine-grained Buffer"
+        unset MPI_ADVANCE_DOUBLE_BUFFERING
+        unset MPI_ADVANCE_FINEGRAIN_MEMORY
+
+        export MPICH_GPU_SUPPORT_ENABLED=1
+        # export MPICH_GPU_IPC_ENABLED=0
+        run_test "mpich" "MPI Single Buffer"            
+        unset MPICH_GPU_SUPPORT_ENABLED
+        # unset MPICH_GPU_IPC_ENABLED
+
+        cat $COLLECTION_DIR/*.tmp >> $TARGET
+        rm -f $COLLECTION_DIR/*.tmp
+    done
+done
