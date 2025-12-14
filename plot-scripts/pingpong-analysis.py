e9d3ae11d80491f2b8d250accd3f1f0062bd9b34
Derek Schafer <schaferderek96@gmail.com>
2025-12-14 14:20:10 -0700

    Added means to get percent differences

--- plot-scripts/pingpong-analysis.py
+++ plot-scripts/pingpong-analysis.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sbn
@@ -19,8 +18,8 @@ df = df[df.Backend != "cxi-fine"]
 df = df.query("not (Backend == 'mpi' and Buffering == 'db')")
 
 # Clean up names
-df['Test Type'] = df['Buffering'] + "_" + df['Backend']
-df['Test Type'] = df['Test Type'].replace({
+df['Test_Type'] = df['Buffering'] + "_" + df['Backend']
+df['Test_Type'] = df['Test_Type'].replace({
                                        "sb_cxi-coarse":"Stream-Triggered Send",
                                        "db_cxi-coarse":"Stream-Triggered Rsend",
                                        "sb_cxi-fine":"Stream-Triggered Send (fine)",
@@ -33,6 +32,32 @@ df['Buffer Size'] = df['Items'] * 4
 df['Bandwidth'] = (df['Iters'] * 2 * df['Buffer Size']) / df['Time'] / 1073741824 
 df['Latency'] = df['Time'] / df['Iters']
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+for key, group in df.groupby("GPU"):
+    cray = group.query("Test_Type == 'Cray MPICH Send'")
+    cray_l = cray.groupby(["Buffer Size"])["Latency"].mean()
+    cray_b = cray.groupby(["Buffer Size"])["Bandwidth"].mean()
+
+    send = group.query("Test_Type == 'Stream-Triggered Send'")
+    send_l = send.groupby(["Buffer Size"])["Latency"].mean()
+    send_b = send.groupby(["Buffer Size"])["Bandwidth"].mean()
+
+    rsend = group.query("Test_Type == 'Stream-Triggered Rsend'")
+    rsend_l = rsend.groupby(["Buffer Size"])["Latency"].mean()
+    rsend_b = rsend.groupby(["Buffer Size"])["Bandwidth"].mean()
+
+    print(key)
+    # Difference to Cray for latency
+    print(((send_l - cray_l) / cray_l) * 100)
+    print(((rsend_l - cray_l) / cray_l) * 100)
+
+    # Difference to Cray for bandwidth
+    print(((send_b - cray_b) / cray_b) * 100)
+    print(((rsend_b - cray_b) / cray_b) * 100)
+
 # Colors to use
 custom_palette = {"Cray MPICH Send": "tab:green",
                   "Stream-Triggered Send":"tab:blue",
@@ -44,7 +69,7 @@ for key, group in df.groupby("GPU"):
     latency_df=group[group['Buffer Size'] < 1048576]
     # Plot
     plt.figure(figsize=(6,4))
-    sbn.lineplot(data=latency_df, x='Buffer Size',y='Latency', hue='Test Type', style='Test Type',
+    sbn.lineplot(data=latency_df, x='Buffer Size',y='Latency', hue='Test_Type', style='Test_Type',
                  errorbar=("ci", 95), markers=True, palette=custom_palette, hue_order=custom_order)
     plt.xscale('log', base=2)
     plt.xlabel("Buffer Size (bytes)")
@@ -56,7 +81,7 @@ for key, group in df.groupby("GPU"):
     plt.savefig(f"pingpong-latency-logY-{key}.png")
 
     plt.figure(figsize=(6,3))
-    sbn.lineplot(data=group, x='Buffer Size',y='Bandwidth', hue='Test Type', style='Test Type',
+    sbn.lineplot(data=group, x='Buffer Size',y='Bandwidth', hue='Test_Type', style='Test_Type',
                  errorbar=("ci", 95), markers=True, palette=custom_palette, hue_order=custom_order)
     plt.xscale('log', base=2)
     plt.xlabel("Buffer Size (bytes)")
@@ -67,9 +92,6 @@ for key, group in df.groupby("GPU"):
     plt.tight_layout()
     plt.savefig(f"pingpong-bandwidth-linearY-{key}.png")
 
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.width', None)
 # latency_pivot_df = pd.pivot_table(df, 
 #                      index = ["Test Type", "Buffer Size"],
 #                      values = ["Latency"],
